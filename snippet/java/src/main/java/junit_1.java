9fe1d86f638e50cc7d09ab0b6c6ac30b91b2bb9e
leaderli <429243408@qq.com>
2022-10-07 15:25:47 +0800

    vault backup: 2022-10-07 15:25:47
    Affected files:
    .obsidian_win/workspace.json
    snippet/java/src/main/java/junit_1.java
    snippet/java/target/classes/ContainerExtension.class
    snippet/java/target/classes/junit_1.class

--- snippet/java/src/main/java/junit_1.java
+++ snippet/java/src/main/java/junit_1.java
@@ -1,13 +1,15 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.*;
 
+import static io.leaderli.litool.core.util.ConsoleUtil.print;
+
 @ExtendWith(ContainerExtension.class)
 public class junit_1 {
 
     @Test
     void test() {
 
-        System.out.println(this);
+        print(this,this.getClass().getClassLoader());
 
     }
 }
