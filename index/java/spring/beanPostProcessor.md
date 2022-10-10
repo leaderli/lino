17d37a613f390ff23f213535e1806e6cc256e023
leaderli <429243408@qq.com>
2022-10-10 17:35:47 +0800

    vault backup: 2022-10-10 17:35:47
    Affected files:
    .obsidian_mac/workspace.json
    index/java/spring/_spring.md
    index/java/spring/beanPostProcessor.md
    snippet/java/src/main/java/bean_definition_registry_post_processor_1.java
    snippet/java/src/main/java/custom_autowired_bean_post_processor_2.java
    snippet/java/target/classes/bean_definition_registry_post_processor_1$Bar.class
    snippet/java/target/classes/bean_definition_registry_post_processor_1$CustomBeanPost.class
    snippet/java/target/classes/bean_definition_registry_post_processor_1$Foo.class
    snippet/java/target/classes/bean_definition_registry_post_processor_1$MyAuto.class
    snippet/java/target/classes/bean_definition_registry_post_processor_1.class
    snippet/java/target/classes/custom_autowired_bean_post_processor_2$Bar.class
    snippet/java/target/classes/custom_autowired_bean_post_processor_2$CustomBeanPost.class
    snippet/java/target/classes/custom_autowired_bean_post_processor_2$Delete.class
    snippet/java/target/classes/custom_autowired_bean_post_processor_2$Foo.class
    snippet/java/target/classes/custom_autowired_bean_post_processor_2$MyAuto.class
    snippet/java/target/classes/custom_autowired_bean_post_processor_2.class

---
+++ index/java/spring/beanPostProcessor.md
@@ -0,0 +1,10 @@
+---
+tags:
+  - java/spring/beanPostProcessor
+---
+
+通过自定义注解来设置成员变量的值
+
+```embed-java
+PATH: "vault://snippet/java/src/main/java/custom_autowired_bean_post_processor_1.java"
+```
--- snippet/java/src/main/java/bean_definition_registry_post_processor_1.java
+++ snippet/java/src/main/java/bean_definition_registry_post_processor_1.java
@@ -1,11 +1,26 @@
-import org.springframework.beans.factory.annotation.Autowired;
+import io.leaderli.litool.core.type.ReflectUtil;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.reflect.Field;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * @author leaderli
  * @since 2022/10/10 8:29 AM
  */
 public class bean_definition_registry_post_processor_1 {
+    @Retention(RUNTIME)
+    @Target({FIELD})
+    public @interface MyAuto {
+
+    }
 
     static class Foo {
 
@@ -13,18 +28,33 @@ public class bean_definition_registry_post_processor_1 {
 
     static class Bar {
 
-        @Autowired
+        @MyAuto
         Foo foo;
+    }
+
+    static class CustomBeanPost implements BeanPostProcessor {
+
+        @Nullable
+        public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+            for (Field field : ReflectUtil.getFields(bean.getClass()).filter(f -> f.isAnnotationPresent(MyAuto.class))) {
+
+                try {
+                    field.set(bean, field.getType().newInstance());
+                } catch (IllegalAccessException | InstantiationException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return bean;
+        }
+
+
     }
 
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.refresh();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CustomBeanPost.class);
         context.registerBean(Bar.class, Bar::new);
-        context.registerBean(Foo.class, Foo::new);
+        assert context.getBean(Bar.class).foo != null;
 
-        System.out.println(context.getBean(Foo.class));
-        System.out.println(context.getBean(Bar.class).foo);
     }
 }
